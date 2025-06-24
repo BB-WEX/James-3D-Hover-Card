05f635516066fb312f234920003c5fa80399b30f
jamesm223 <jamesmorton223@outlook.com>
2025-06-24 15:00:52 +0100

    CHanged the background

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function showToggle() {
     var hide = document.getElementById('hdn-txt')
     var large = document.getElementById('content')
     var bottom = document.getElementById('btm-cont')
+    var back = document.getElementById('back')
     hide.classList.toggle('hidden')
     hide.classList.toggle('enlarge')
     large.classList.toggle('enlarge')
     bottom.classList.toggle('enlarge')
+    back.classList.toggle('enlarge')
 }
 
 const button = document.getElementById("btn-id");
